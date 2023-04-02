eac2c88c4120dec42d39d8a352f408e8b76b9480
Collinsolayemi <collinsolayemi@gmail.com>
2023-04-01 19:03:24 -0700

    pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Importing our dependencies
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
@@ -7,7 +8,14 @@ const port = process.env.PORT || 2003;
 
 //Telling the view engine we using
 app.set('view engine', 'pug');
+app.set('views', 'views');
 
+//Rendring the pug file to the client
+app.get('/', (req, res, next) => {
+  res.status(200).render('home');
+});
+
+//Creating the server
 const server = app.listen(port, () => {
   console.log(`server started successfully on port ${port}`);
 });
